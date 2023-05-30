c91ceb5a0cbadb32de7870f3d06deace470bb569
lechenco <glechenco@gmail.com>
2023-05-30 20:07:16 -0300

    feat: add border helpers and tests

---
+++ app/src/main/java/boustrophedon/helpers/primitives/BorderHelper.java
@@ -0,0 +1,77 @@
+package boustrophedon.helpers.primitives;
+
+import java.util.ArrayList;
+
+import boustrophedon.domain.primitives.model.IBorder;
+import boustrophedon.domain.primitives.model.IPoint;
+import boustrophedon.domain.walkers.model.WalkerConfig;
+import boustrophedon.provider.primitives.Border;
+import boustrophedon.provider.primitives.Point;
+import boustrophedon.utils.GA;
+
+public class BorderHelper {
+    static public IPoint calcIntersectionToWall(IPoint currentPoint, IBorder wall, double angle) {
+        if (wall.isOnBorder(currentPoint)) return currentPoint;
+
+        IBorder refBorder = new Border(new Point(0, 0), new Point(Math.cos(angle), Math.sin(angle)));
+
+        if (wall.isParallelToY()) {
+            return calcIntersectionWithWallParallelToY(currentPoint, wall, refBorder);
+        } else if (refBorder.isParallelToY()) {
+            return calcIntersectionWithRefParallelToY(currentPoint, wall);
+        } else {
+            return calcIntersection(currentPoint, refBorder, wall);
+        }
+    }
+
+    static public IPoint calcIntersectionWithWallParallelToY(IPoint currentPoint, IBorder wall, IBorder refBorder) {
+        if (refBorder.isParallelToY())
+            return calcIntersectionWithRefParallelToY(currentPoint, wall);
+        double intersectionX, intersectionY;
+        double[] refBorderCoefficients = refBorder.getParallelLineCoefficients(currentPoint);
+        intersectionX = wall.getFirstVertice().getX();
+        intersectionY = GA.calcYPoint(refBorderCoefficients, intersectionX);
+
+        return new Point(intersectionX, intersectionY);
+    }
+
+    static public IPoint calcIntersectionWithRefParallelToY(IPoint currentPoint, IBorder wall) {
+        if (wall.isParallelToY())
+            return new Point(wall.getFirstVertice().getX(), currentPoint.getY());
+
+        double intersectionX, intersectionY;
+        double[] wallCoefficients = wall.getCoefficients();
+
+        intersectionX = currentPoint.getX();
+        intersectionY = GA.calcYPoint(wallCoefficients, intersectionX);
+
+        return new Point(intersectionX, intersectionY);
+    }
+
+    static public IPoint calcIntersection(IPoint currentPoint, IBorder refBorder, IBorder wall) {
+        double intersectionX, intersectionY;
+        double[] refBorderCoefficients = refBorder.getParallelLineCoefficients(currentPoint);
+        double[] wallCoefficients = wall.getCoefficients();
+
+        intersectionX = Math.abs(wall.getAngleFirstHalf() - refBorder.getAngleFirstHalf()) < WalkerConfig.ANGLE_PRECISION ?
+                currentPoint.getX() :
+                (refBorderCoefficients[1] - wallCoefficients[1]) /
+                        (wallCoefficients[0] - refBorderCoefficients[0]);
+        intersectionY = GA.calcYPoint(wallCoefficients, intersectionX);
+
+        return new Point(intersectionX, intersectionY);
+    }
+
+    static public ArrayList<IBorder> findWalls(ArrayList<IBorder> borders, double angle) {
+        ArrayList<IBorder> walls = new ArrayList<>();
+
+        double angleSanitized = GA.getFirstHalfAngle(angle);
+
+        for (IBorder border : borders) {
+            if (Math.abs(angleSanitized - border.getAngleFirstHalf()) > WalkerConfig.ANGLE_PRECISION)
+                walls.add(border);
+        }
+        return walls;
+    }
+
+}
