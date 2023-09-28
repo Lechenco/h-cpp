48872431c8015ddd83f73e3777b7059657e9b7ef
Gabriel Lechenco <glechenco@gmail.com>
2023-09-28 20:46:08 -0300

    feat: decompose cells and plot at screen

--- app/src/main/java/com/dji/gsdemo/gmapsteste/presentation/view/activity/MapsActivity.java
+++ app/src/main/java/com/dji/gsdemo/gmapsteste/presentation/view/activity/MapsActivity.java
@@ -3,17 +3,24 @@ package com.dji.gsdemo.gmapsteste.presentation.view.activity;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.dji.gsdemo.gmapsteste.R;
 import com.dji.gsdemo.gmapsteste.adapter.map.PolygonAdapter;
+import com.dji.gsdemo.gmapsteste.controller.coveragePathPlanning.DecomposerController;
 import com.dji.gsdemo.gmapsteste.controller.map.MapController;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.dji.gsdemo.gmapsteste.databinding.ActivityMapsBinding;
 
+import java.util.ArrayList;
+
+import boustrophedon.domain.decomposer.error.ExceedNumberOfAttempts;
+import boustrophedon.domain.decomposer.model.ICell;
 import boustrophedon.provider.primitives.Point;
 import boustrophedon.provider.primitives.Polygon;
 
@@ -49,26 +56,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mapController = new MapController(googleMap);
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker in Cornélio and move the camera
         LatLng cornelio = new LatLng(-23.1813, -50.648);
-//        mMap.addMarker(new MarkerOptions().position(cornelio).title("Cornélio Procópio"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(cornelio));
-//        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
-//        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style));
+//        googleMap.addMarker(new MarkerOptions().position(cornelio).title("Cornélio Procópio"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(cornelio));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+//        googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style));
 
 
         double squareSize = 0.005;
 
         double angle = Math.PI / 4;
 
-        Polygon square = new Polygon(
-                new Point(cornelio.latitude - 2 * squareSize, cornelio.longitude),
-                new Point(cornelio.latitude, cornelio.longitude + 2 * squareSize),
+//        Polygon square = new Polygon(
+//                new Point(cornelio.latitude - 2 * squareSize, cornelio.longitude),
+//                new Point(cornelio.latitude, cornelio.longitude + 2 * squareSize),
 //                new Point(cornelio.latitude + 2 * squareSize, cornelio.longitude),
-                new Point(cornelio.latitude, cornelio.longitude - 2 * squareSize)
-        );
+//                new Point(cornelio.latitude, cornelio.longitude - 2 * squareSize)
+//        );
 
-        mapController.addPolygon(PolygonAdapter.toPolygonOptions(square));
+        Polygon middleOutPolygon = new Polygon(
+                new Point(cornelio.latitude + 0, cornelio.longitude + 0),
+                new Point(cornelio.latitude + 1, cornelio.longitude + 0),
+                new Point(cornelio.latitude + 1.5, cornelio.longitude + 0.5), // MIDDLE Event
+                new Point(cornelio.latitude + 2, cornelio.longitude + 0),
+                new Point(cornelio.latitude + 3, cornelio.longitude + 0),
+                new Point(cornelio.latitude + 3, cornelio.longitude + 3),
+                new Point(cornelio.latitude + 0, cornelio.longitude + 3),
+                new Point(cornelio.latitude + 0, cornelio.longitude + 2),
+                new Point(cornelio.latitude + 0.5, cornelio.longitude + 1.5), // OUT Event
+                new Point(cornelio.latitude + 0, cornelio.longitude + 1)
+        );
+        DecomposerController decomposerController = new DecomposerController();
+        try {
+            ArrayList<ICell> cells = decomposerController.decompose(middleOutPolygon);
+            cells.forEach(cell -> mapController
+                    .addPolygon(
+                            PolygonAdapter
+                                    .toPolygonOptions((Polygon) cell.getPolygon())
+                                    .fillColor(Color.argb(33, 0, 0, 200))
+                    ));
+        } catch (ExceedNumberOfAttempts e) {
+//            throw new RuntimeException(e);
+            mapController.addPolygon(PolygonAdapter.toPolygonOptions(middleOutPolygon)
+                    .fillColor(Color.argb(33, 0, 200, 0))
+            );
+        }
 
 //        for (LatLng p : polylineOptions.getPoints()) {
 //            mMap.addMarker(new MarkerOptions().position(p));
