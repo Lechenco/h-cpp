cf51a97b9bf6256653540bf1f0ec75524b9fe124
Gabriel Lechenco <glechenco@gmail.com>
2023-10-23 19:56:38 -0300

    fix: final cells

--- app/src/main/java/boustrophedon/provider/decomposer/Boustrophedon/Splitters/NoneSplitter.java
+++ app/src/main/java/boustrophedon/provider/decomposer/Boustrophedon/Splitters/NoneSplitter.java
@@ -1,31 +1,34 @@
 package boustrophedon.provider.decomposer.Boustrophedon.Splitters;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
+import boustrophedon.domain.decomposer.enums.Events;
+import boustrophedon.domain.decomposer.error.ExceedNumberOfAttempts;
 import boustrophedon.domain.decomposer.model.ICell;
 import boustrophedon.domain.decomposer.model.ICriticalPoint;
-import boustrophedon.domain.decomposer.model.ISplitter;
 import boustrophedon.provider.decomposer.Boustrophedon.Cell.CellHelper;
 import boustrophedon.provider.decomposer.Boustrophedon.CriticalPoint.CriticalPoint;
 
-public class NoneSplitter implements ISplitter {
-    private ArrayList<ICell> cells;
-    private final ArrayList<CriticalPoint> criticalPoints;
+public class NoneSplitter extends OutSplitter {
 
     public NoneSplitter(ArrayList<CriticalPoint> criticalPoints) {
-        this.criticalPoints = criticalPoints;
+        super(criticalPoints);
     }
 
     @Override
-    public void split(ICriticalPoint splitPoint) {
+    public void split(ICriticalPoint splitPoint) throws ExceedNumberOfAttempts {
         this.cells = new ArrayList<>();
-        ICell cell = CellHelper.createCell(this.criticalPoints);
-        this.cells.add(cell);
-    }
 
-    @Override
-    public ArrayList<ICell> getCells() {
-        return this.cells;
+        Optional<CriticalPoint> inPointOptional = this
+                .criticalPoints.stream().filter(cp -> cp.getEvent() == Events.IN)
+                .findFirst();
+        if (inPointOptional.isPresent()) {
+            this.populateCells(this.criticalPoints, inPointOptional.get());
+        } else {
+            ICell cell = CellHelper.createCell(this.criticalPoints);
+            this.cells.add(cell);
+        }
     }
 
     @Override
--- app/src/test/java/boustrophedon/controllers/decomposer/Boustrophedon/Splitters/SplitterControllerIntegrationTest.java
+++ app/src/test/java/boustrophedon/controllers/decomposer/Boustrophedon/Splitters/SplitterControllerIntegrationTest.java
@@ -5,18 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-import boustrophedon.domain.decomposer.enums.Events;
 import boustrophedon.domain.decomposer.error.ExceedNumberOfAttempts;
 import boustrophedon.domain.decomposer.model.ICell;
-import boustrophedon.domain.primitives.model.IBorder;
-import boustrophedon.domain.primitives.model.IPoint;
 import boustrophedon.domain.primitives.model.IPolygon;
 import boustrophedon.factories.decomposer.Boustrophedon.CriticalPoint.CriticalPointFactory;
-import boustrophedon.provider.Cell;
 import boustrophedon.provider.decomposer.Boustrophedon.CriticalPoint.CriticalPoint;
-import boustrophedon.provider.decomposer.Boustrophedon.CriticalPoint.CriticalPointerHelper;
 import boustrophedon.provider.graph.MatrixAdjacency;
 import boustrophedon.provider.graph.Node;
 import boustrophedon.provider.primitives.Point;
@@ -92,6 +86,28 @@ public class SplitterControllerIntegrationTest {
 
         MatrixAdjacency<Node<ICell>> matrix = splitterController.execute();
         assertEquals(3, matrix.getNodes().size());
+    }
+    @Test
+    public void executeInEvent2() throws ExceedNumberOfAttempts {
+        IPolygon polygon = new Polygon(
+                new Point(0, 0),
+                new Point(2.6, 0),
+                new Point(2.7, 0.8), // MIDDLE Event
+                new Point(2.8, 0),
+                new Point(3, 0),
+                new Point(3, 1),
+                new Point(2.5, 1.5), // IN Event
+                new Point(3, 2),
+                new Point(3, 3),
+                new Point(0, 3)
+        );
+
+        ArrayList<CriticalPoint> cps = CriticalPointFactory.execute(polygon);
+
+        SplitterController splitterController = new SplitterController(cps);
+
+        MatrixAdjacency<Node<ICell>> matrix = splitterController.execute();
+        assertEquals(4, matrix.getNodes().size());
     }
     @Test
     public void executeOUTEvent() throws ExceedNumberOfAttempts {
